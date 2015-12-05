c9d8526b3af286536470f0a50efba050024cc0ae
F4r3n <guillaume_k@hotmail.fr>
2015-12-05 22:10:45 +0100

    Builder images

--- app/src/main/java/com/example/guillaume2/gw2applicaton/Builder/DialogSpeChoice.java
+++ app/src/main/java/com/example/guillaume2/gw2applicaton/Builder/DialogSpeChoice.java
@@ -4,18 +4,19 @@ import android.app.Activity;
 import android.app.DialogFragment;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -39,6 +40,8 @@ public class DialogSpeChoice extends DialogFragment implements
     Activity activity;
     CallerBack parent;
     FrameLayout frameLayout;
+    final int sdk = android.os.Build.VERSION.SDK_INT;
+
 
     public DialogSpeChoice() {
     }
@@ -76,7 +79,6 @@ public class DialogSpeChoice extends DialogFragment implements
         Bitmap bitmap = b;
 
 
-
         // Get current dimensions AND the desired bounding box
         int width = 0;
 
@@ -89,9 +91,6 @@ public class DialogSpeChoice extends DialogFragment implements
         int height = bitmap.getHeight();
         int boundingX = dpToPx(w);
         int boundingY = dpToPx(h);
-        Log.i("Test", "original width = " + Integer.toString(width));
-        Log.i("Test", "original height = " + Integer.toString(height));
-        Log.i("Test", "bounding = " + Integer.toString(boundingX));
 
         // Determine how much to scale: the dimension requiring less scaling is
         // closer to the its side. This way the image always stays inside your
@@ -99,9 +98,6 @@ public class DialogSpeChoice extends DialogFragment implements
         float xScale = ((float) boundingX) / width;
         float yScale = ((float) boundingY) / height;
         float scale = (xScale <= yScale) ? xScale : yScale;
-        Log.i("Test", "xScale = " + Float.toString(xScale));
-        Log.i("Test", "yScale = " + Float.toString(yScale));
-        Log.i("Test", "scale = " + Float.toString(scale));
 
         // Create a matrix for the scaling and add the scaling data
         Matrix matrix = new Matrix();
@@ -112,8 +108,7 @@ public class DialogSpeChoice extends DialogFragment implements
         width = scaledBitmap.getWidth(); // re-use
         height = scaledBitmap.getHeight(); // re-use
         BitmapDrawable result = new BitmapDrawable(scaledBitmap);
-        Log.i("Test", "scaled width = " + Integer.toString(width));
-        Log.i("Test", "scaled height = " + Integer.toString(height));
+
 
         // Apply the scaled bitmap
         view.setImageDrawable(result);
@@ -124,7 +119,6 @@ public class DialogSpeChoice extends DialogFragment implements
         params.height = height;
         view.setLayoutParams(params);
 
-        Log.i("Test", "done");
     }
 
     private int dpToPx(int dp) {
@@ -135,17 +129,17 @@ public class DialogSpeChoice extends DialogFragment implements
     public float convertDpToPixels(float dp) {
         Resources resources = activity.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
-        float px = dp * (metrics.densityDpi / 160f);
-        return px;
+        return dp * (metrics.densityDpi / 160f);
     }
 
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         ImageView imageView = (ImageView) frameLayout.findViewById(R.id.background);
+        frameLayout.setVisibility(View.VISIBLE);
         Bitmap b = FileManagerTool.getImage(
                 specializations.get(position).specializationData.backgroundImage.iconPath);
-        if(!specializations.get(position).specializationData.elite) {
+        if (!specializations.get(position).specializationData.elite) {
 
             System.out.println(convertDpToPixels(1500));
             b = Bitmap.createBitmap(b, 0, 128, 800, 128);
@@ -156,38 +150,55 @@ public class DialogSpeChoice extends DialogFragment implements
         }
 
 
+        List<ImageButton> buttonsMinor = new ArrayList<>();
+        buttonsMinor.add((ImageButton) frameLayout.findViewById(R.id.t10));
+        buttonsMinor.add((ImageButton) frameLayout.findViewById(R.id.t20));
+        buttonsMinor.add((ImageButton) frameLayout.findViewById(R.id.t30));
 
-        List<Button> buttonsMinor = new ArrayList<>();
-        buttonsMinor.add((Button) frameLayout.findViewById(R.id.t10));
-        buttonsMinor.add((Button) frameLayout.findViewById(R.id.t20));
-        buttonsMinor.add((Button) frameLayout.findViewById(R.id.t30));
-
-        for(int i = 0; i < buttonsMinor.size(); ++i) {
+        for (int i = 0; i < buttonsMinor.size(); ++i) {
             Bitmap bb = FileManagerTool.getImage(
                     specializations.get(position).specializationData.minorTraits.get(i).iconImage.iconPath);
-            buttonsMinor.get(i).setBackgroundDrawable(new BitmapDrawable(activity.getResources(),bb));
+            buttonsMinor.get(i).setImageBitmap(makeTransparent(bb, 255));
+
         }
 
-        List<Button> buttonsMajor = new ArrayList<>();
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t11));
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t12));
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t13));
+        List<ImageButton> buttonsMajor = new ArrayList<>();
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t11));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t12));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t13));
 
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t21));
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t22));
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t23));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t21));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t22));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t23));
 
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t31));
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t32));
-        buttonsMajor.add((Button) frameLayout.findViewById(R.id.t33));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t31));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t32));
+        buttonsMajor.add((ImageButton) frameLayout.findViewById(R.id.t33));
 
-        for(int i = 0; i < buttonsMajor.size(); ++i) {
+        for (int i = 0; i < buttonsMajor.size(); ++i) {
             Bitmap bb = FileManagerTool.getImage(
                     specializations.get(position).specializationData.majorTraits.get(i).iconImage.iconPath);
-            buttonsMajor.get(i).setBackgroundDrawable(new BitmapDrawable(activity.getResources(), bb));
+           buttonsMajor.get(i).setImageBitmap(makeTransparent(bb, 255));
+
         }
 
 
         dismiss();
     }
+
+    public Bitmap makeTransparent(Bitmap src, int value) {
+        if(value == 255) return src;
+        int width = src.getWidth();
+        int height = src.getHeight();
+        Bitmap transBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(transBitmap);
+        canvas.drawARGB(0, 0, 0, 0);
+        // config paint
+        final Paint paint = new Paint();
+        paint.setAlpha(value);
+        canvas.drawBitmap(src, 0, 0, paint);
+        return transBitmap;
+    }
+
+
 }
--- app/src/main/java/com/example/guillaume2/gw2applicaton/BuilderActivity.java
+++ app/src/main/java/com/example/guillaume2/gw2applicaton/BuilderActivity.java
@@ -2,6 +2,8 @@ package com.example.guillaume2.gw2applicaton;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -23,10 +25,13 @@ public class BuilderActivity extends AppCompatActivity implements CallerBack {
     private SpecializationManager specializationManager;
     private List<Specialization> specializations;
     private DialogSpeChoice dialogSpeChoice;
+    private Professions professions;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         dialogSpeChoice = new DialogSpeChoice();
         Collection c = (Collection) getApplication();
@@ -40,6 +45,67 @@ public class BuilderActivity extends AppCompatActivity implements CallerBack {
 
         specialization2 = (FrameLayout) findViewById(R.id.spe2);
         specialization3 = (FrameLayout) findViewById(R.id.spe3);
+        changeVisibility(View.INVISIBLE);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+    }
+
+    private void changeVisibility(int visibility) {
+        specialization1.setVisibility(visibility);
+        specialization2.setVisibility(visibility);
+        specialization3.setVisibility(visibility);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        boolean in = false;
+        switch (id) {
+            case R.id.action_warrior:
+                professions = Professions.WARRIOR;
+                in = true;
+                break;
+            case R.id.action_elementalist:
+                professions = Professions.ELEMENTALIST;
+                in = true;
+                break;
+            case R.id.action_guardian:
+                professions = Professions.GUARDIAN;
+                in = true;
+                break;
+            case R.id.action_mesmer:
+                professions = Professions.MESMER;
+                in = true;
+                break;
+            case R.id.action_necro:
+                professions = Professions.NECROMANCER;
+                in = true;
+                break;
+            case R.id.action_revenant:
+                professions = Professions.REVENANT;
+                in = true;
+                break;
+            case R.id.action_thief:
+                professions = Professions.THIEF;
+                in = true;
+                break;
+            case R.id.action_ranger:
+                professions = Professions.RANGER;
+                in = true;
+                break;
+
+        }
+
+        setTitle(professions.toString().toLowerCase());
+        HashMap<String, Specialization> spe = specializationManager.getSpe().get(professions);
+        specializations = new ArrayList<>(spe.values());
+        if (in) {
+            changeVisibility(View.INVISIBLE);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
     }
 
     public void changeSpe(View view) {
@@ -47,7 +113,6 @@ public class BuilderActivity extends AppCompatActivity implements CallerBack {
             case R.id.speChange1:
                 dialogSpeChoice.init(this, specializations, this, specialization1);
                 dialogSpeChoice.show(getFragmentManager(), "dialogSpeChoice");
-                System.out.println("Choice 1");
                 break;
             case R.id.speChange2:
                 dialogSpeChoice.init(this, specializations, this, specialization2);
@@ -60,17 +125,21 @@ public class BuilderActivity extends AppCompatActivity implements CallerBack {
         }
     }
 
-    private void sortSpe() {
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.builder_activity_menu, menu);
+        return true;
     }
+
 
     @Override
     public void notifyUpdate(Object... o) {
-
     }
 
     @Override
     public void cancel() {
-
     }
+
+
 }
