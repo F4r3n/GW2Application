be6e8c67dbf2dc696b39c6cc507c40c48ae45bba
F4r3n <guillaume_k@hotmail.fr>
2016-01-25 20:13:38 +0100

    Advanced research item activity

--- database/db.py
+++ database/db.py
@@ -3,7 +3,7 @@ import urllib
 import json
 import sqlite3
 import os
-
+from shutil import copyfile
 
 nameDatabase = "gw2item.db"
 def createDatabase():
@@ -13,7 +13,15 @@ def createDatabase():
     c.execute("drop table if exists gwitem")
     conn.commit()
     c.execute('''CREATE TABLE gwitem
-             (rid INTEGER, name text, description text, iconUrl text, image bytea)''')
+             (rid INTEGER, 
+                name text, 
+              description text,
+                type text,
+                rarity text,
+                level INTEGER,
+                vendor_value INTEGER,
+                infoWeb text,
+              iconUrl text, image bytea)''')
 
     conn.commit()
     conn.close()
@@ -31,7 +39,7 @@ def prepareForAndroid():
     conn.close()
 
 def insertItem(cursor, table):
-    cursor.executemany("INSERT INTO gwitem ('rid','name','description', 'iconUrl') VALUES (?,?,?,?)", table)
+    cursor.executemany("INSERT INTO gwitem ('rid','name','description', 'type','rarity','level','vendor_value','infoWeb','iconUrl') VALUES (?,?,?,?,?, ?, ?, ?, ?)", table)
 
 def fillDatabase():
     percentage = 0
@@ -40,7 +48,7 @@ def fillDatabase():
     data = response.read()
     text = data.decode('utf-8')
 
-    conn = sqlite3.connect(nameDatabase, timeout=2)
+    conn = sqlite3.connect(nameDatabase, timeout=100)
     cursor = conn.cursor()
     listIds = json.loads(text)
 
@@ -61,24 +69,38 @@ def fillDatabase():
         name = ""
         id = ""
         iconUrl = ""
+        level = 0
+        rarity = ""
+        type = "",
+        vendor_value = 0
+        infoWeb = ""
         for object in jsonObject:
             if 'description' in object:
                 description = object['description']
-
             if 'name' in object:
                 name = object['name']
-
             if 'id' in object:
                 id = object['id']
             if 'icon' in object:
                 iconUrl = object['icon']
+            if 'type' in object:
+                type = object['type']
+
+            if 'rarity' in object:
+                rarity = object['rarity']
+            if 'level' in object:
+                level = object['level']
+
+            if 'vendor_value' in object:
+                vendor_value = object['vendor_value']
+
             path = "images/" + name + ".png"
            # if not os.path.isfile(path):
             #    urllib.request.urlretrieve(iconUrl, path)
-            table.append((id, name, description, iconUrl))
+            table.append((id, name, description, type, rarity, level, vendor_value, infoWeb, iconUrl))
             index = index + 1
             percentage = (index*100)/len(listIds)
-            print(percentage)
+        print(str(percentage)+"\r")
 
         insertItem(cursor,table) 
         conn.commit()
@@ -89,4 +111,4 @@ def fillDatabase():
 createDatabase()
 prepareForAndroid()
 fillDatabase()
-#os.rename(nameDatabase, "../app/src/main/assets/databases" + nameDatabase)
+copyfile(nameDatabase, "../app/src/main/assets/databases/" + nameDatabase)
